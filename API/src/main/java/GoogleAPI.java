64351083e55278fac060389ef52026750759e21d
qianboao <qianboao@outlook.com>
2024-11-23 00:55:59 +0800

    to do 优化请求逻辑

--- API/src/main/java/GoogleAPI.java
+++ API/src/main/java/GoogleAPI.java
@@ -1,3 +1,4 @@
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -35,39 +36,80 @@ public class GoogleAPI {
         return proxy_host + ":" + proxy_port;
     }
 
-    public ArrayList<Paper> GetByName(String name) throws IOException {
-        ArrayList<Paper> papers = new ArrayList<>();
-            try {
-                String url = now_url + "?hl=zh-CN&q=" + name.replace(" ", "+");
-                Document doc;
-                if (this.proxy_port>0){
-                    doc = Jsoup.connect(url).proxy(this.proxy_host, this.proxy_port).get();
-                }
-                else {
-                    doc = Jsoup.connect(url).get();
-                }
-                Elements result = doc.selectXpath("//*[@id=\"gs_res_ccl_mid\"]").getFirst().getElementsByClass("gs_ri");
-                for (Element element : result) {
-                    String title = element.getElementsByClass("gs_rt").getFirst().getElementsByTag("a").text();
-                    String author = element.getElementsByClass("gs_a").text();
-                    String cite_url = element.selectXpath("div[3]/a[4]").attr("href");
-                    if (!cite_url.isEmpty()) {
-                        cite_url = now_url.substring(0, now_url.indexOf("/", 9)) + cite_url;
-                    }
-                    papers.add(new Paper(title, author, cite_url));
-                }
-            } catch (IOException e) {
-                System.out.println("从url: " + now_url+"\n获取信息失败");
-                e.printStackTrace();
+    public String ConstructURL(String url, String title, String lang, int start) {
+
+        String result;
+        if (start==0){
+            result = "%s?hl=%s&q=%s".formatted(url,lang,title.replace(" ", "+"));
+        }
+        else {
+            result = "%s?start=%d&hl=%s&q=%s".formatted(url,start,lang,title.replace(" ", "+"));
+        }
+        return result;
+    }
+
+    public String ConstructURL(String url, String title, String lang) {
+        return ConstructURL(url,title,lang,0);
+    }
+
+    public String ConstructURL(String url, String title) {
+        return ConstructURL(url,title,"zh-CN",0);
+    }
+
+    public ArrayList<Paper> GetByName(String name,int max_items) throws IOException {
+        ArrayList<Paper> papers = null;
+        try {
+            String url = ConstructURL(now_url,name);
+            Document doc;
+            Connection connection = Jsoup.connect(url);
+            // 判断是否需要使用代理
+            if (this.proxy_port > 0) {
+                connection = connection.proxy(this.proxy_host, this.proxy_port);
             }
+            // 执行请求并获取响应
+            Connection.Response response = connection.execute();
+            // 检查响应码
+            if (response.statusCode() != 200) {
+                throw new IOException("HTTP请求失败，状态码：" + response.statusCode());
+            }
+            papers = ParsePaper(response.parse());
+        } catch (IOException e) {
+            System.out.println("从url: " + now_url + "\n获取信息失败");
+            e.printStackTrace();
+        }
         return papers;
     }
 
+    public ArrayList<Paper> GetByName(String name) throws IOException {
+        return GetByName(name,10);
+    }
+
+    public ArrayList<Paper> ParsePaper(Document doc) {
+        //提取页面的paper信息,返回信息列表
+        ArrayList<Paper> papers = new ArrayList<>();
+        Elements result = doc.getElementsByClass("gs_r gs_or gs_scl");
+        for (Element element : result) {
+            String title = element.getElementsByClass("gs_rt").getFirst().getElementsByTag("a").text();
+            String author = element.getElementsByClass("gs_a").text();
+            String cited_url = element.selectXpath("div[2]/div[3]/a[3]").attr("href");
+            String relation_url = element.selectXpath("div[2]/div[3]/a[4]").attr("href");
+            String pdf_url = element.selectXpath("div[1]/div/div/a").attr("href");
+            if (!relation_url.isEmpty()) {
+                relation_url = now_url.substring(0, now_url.indexOf("/", 9)) + relation_url;
+            }
+            if (!cited_url.isEmpty()) {
+                cited_url = now_url.substring(0, now_url.indexOf("/", 9)) + cited_url;
+            }
+            papers.add(new Paper(title, author, relation_url, cited_url, pdf_url));
+        }
+        return papers;
+    }
+
+
     public static void main(String[] args) throws IOException {
         GoogleAPI api = new GoogleAPI();
-        api.setProxy("127.0.0.1",7890);
-        api.setNow_url(1);
-        ArrayList<Paper> papers = api.GetByName("attention is all you need");
+        api.setNow_url(0);
+        ArrayList<Paper> papers = api.GetByName("attention is all you need",10);
         System.out.println(papers.stream().map(Paper::toString).collect(Collectors.joining()));
     }
 }
--- Paper/src/main/java/qba/Paper.java
+++ Paper/src/main/java/qba/Paper.java
@@ -1,16 +1,48 @@
 package qba;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Paper{
     private final String title;
     private final String authorInfo;
-    private String relation_url;
+    private final String relation_url;
+    private final String cited_url;
+    private final String pdf_url;
+    private final int year;
     private ArrayList<Paper> relations = new ArrayList<Paper>();
-    public Paper(String title, String author,String relation_url){
+    public Paper(String title, String info,String relation_url,String cited_url,String pdf_url){
         this.title = title;
-        this.authorInfo = author;
-        this.relation_url = relation_url;
+        String[] info_list = info.split(",");
+        this.authorInfo = info_list[0];
+        String regex = "\\b\\d{4} -\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(info);
+        if (matcher.find()) {
+            this.year = Integer.parseInt(matcher.group());
+        }
+        else {
+            this.year = -1;
+        }
+        if (relation_url!=null){
+            this.relation_url = relation_url;
+        }
+        else{
+            this.relation_url = null;
+        }
+        if (cited_url!=null){
+            this.cited_url = cited_url;
+        }
+        else{
+            this.cited_url = null;
+        }
+        if (cited_url!=null){
+            this.pdf_url = pdf_url;
+        }
+        else{
+            this.pdf_url = null;
+        }
     }
     public void addRelation(Paper relation){
         relations.add(relation);
@@ -27,9 +59,45 @@ public class Paper{
     public String getAuthor(){
         return authorInfo;
     }
+    public String getYear(){
+        if (year==-1){
+            return "unknow";
+        }
+        return String.valueOf(year);
+    }
+    public boolean isComplated(){
+        if (year==-1){
+            return false;
+        }
+        if (cited_url.length()<9){
+            return false;
+        }
+        if (relation_url.length()<9){
+            return false;
+        }
+        if (pdf_url.length()<9){
+            return false;
+        }
+        return true;
+    }
     public String getRelation_url(){return relation_url;}
     public String toString(){
-        String info = "\n"+"-".repeat(20)+"\n"+getTitle()+"\n"+getAuthor()+"\n"+getRelation_url()+"\n"+"-".repeat(20);
+        var info = "";
+        info += "Title: " + title + "\n";
+        info += "Author: " + authorInfo + "\n";
+        if(year>0){
+            info += "Year: " + year + "\n";
+        }
+        if (relation_url.length()>9){
+            info += "Relation: " + relation_url + "\n";
+        }
+        if (cited_url.length()>9){
+            info += "Cited: " + cited_url + "\n";
+        }
+        if (pdf_url.length()>9){
+            info += "PDF: " + pdf_url + "\n";
+        }
+        info += "*".repeat(20)+"\n";
         return info;
     }
 }
